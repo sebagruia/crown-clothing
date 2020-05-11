8dcdd9088010badc8a8a3d9c0ed5f16f804e5a83
sebagruia <sebastiangruia@gmail.com>
2020-05-11 11:43:31 +0200

    Removed redux-logger from production

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,14 @@ import thunkMiddleware from "redux-thunk";
 import logger from 'redux-logger';
 import rootReducer from "./root-reducer";
 
- export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
+const middlewares = [];
+
+// If we are not in Development mode the "logger" middleware is used, if in Production mode the "logger" middleware is not active
+if(process.env.NODE_ENV === "development"){
+    middlewares.push(logger)
+}
+
+ export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, ...middlewares));
 
 // We are caching the store in the localStorage using the "redux-persist" - persistStore() function;
  export const persistor = persistStore(store); 
