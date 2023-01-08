e9b2d5a380ec84e4da26b46b3a6b916c3666d688
sebagruia <blackthorus@gmail.com>
2023-01-08 15:55:53 +0200

    Add baseRoot

--- client/src/index.js
+++ client/src/index.js
@@ -15,7 +15,7 @@ const root = createRoot(container);
 root.render(
   <Provider store={store}>
     <React.StrictMode>
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         {/* <PersistGate persistor={persistor}> */}
           <App />
         {/* </PersistGate> */}
